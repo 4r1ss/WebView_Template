1c167074519e3a3faabeb44045e895be0b792167
i-madea-thing <aristsiolpidis@protonmail.com>
2022-06-09 17:58:00 +0300

    WebView Code

---
+++ WebView/app/src/main/java/com/example/webview_template/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.webview_template;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://github.com/4r1ss");   //Your link
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);    //Enables JavaScript
+    }
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
